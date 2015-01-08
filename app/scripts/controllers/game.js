dc56eeecf05f94c31d6cb797285d09eab27e76ce
Md Ali Ahsan Rana <rana_cse_ruet@yahoo.com>
2015-01-08 13:44:44 -0500

    * minor refactorings.

--- app/scripts/controllers/game.js
+++ app/scripts/controllers/game.js
@@ -1,22 +1,23 @@
 'use strict';
 
 /**
- * Definition of the board class
+ * Definition of the Board class
  * @param dimension
  * @param numOfMine
  * @constructor
  */
-function Board(dimension, numOfMine)
+function Board(dimension, numOfMine, edgeTraveler)
 {
   this.boardMatrix = [];
   this.dimension = dimension;
   this.numOfMine = numOfMine;
 
+  var me = this;
   //update a specified box counter
   this.updateCounter = function(x, y) {
-    if(x >=0 && x< this.dimension && y>=0 &&
-       y<this.dimension && this.boardMatrix[x][y]!=='*') {
-      this.boardMatrix[x][y]++;
+    if(x >=0 && x< me.dimension && y>=0 &&
+       y<me.dimension && me.boardMatrix[x][y]!=='*') {
+      me.boardMatrix[x][y]++;
     }
   };
 
@@ -30,7 +31,7 @@ function Board(dimension, numOfMine)
   }
 
   //set specified number of mines
-  for(i=0; i<this.numOfMine;i++){
+  for(i=0; i<this.numOfMine;i++) {
     var x = parseInt((Math.random()*10)%this.dimension);
     var y = parseInt((Math.random()*10)%this.dimension);
     if(this.boardMatrix[x][y] === '*'){
@@ -42,53 +43,54 @@ function Board(dimension, numOfMine)
     this.boardMatrix[x][y] = '*';
 
     //update all 8 attached box counters
-    this.updateCounter(x-1,y-1);
-    this.updateCounter(x-1,y);
-    this.updateCounter(x-1,y+1);
-    this.updateCounter(x,y-1);
-    this.updateCounter(x,y+1);
-    this.updateCounter(x+1,y-1);
-    this.updateCounter(x+1,y);
-    this.updateCounter(x+1,y+1);
+    edgeTraveler(x, y, this.updateCounter);
   }
 }
 
-function GameController($scope)
+/**
+ * Definition for main Game Controller class
+ * @param $scope
+ * @param $rootScope
+ * @param edgeTraveler
+ * @constructor
+ */
+function GameController($scope, $rootScope, edgeTraveler)
 {
   var numOfMine = 8;
   $scope.dimension = 8;
   $scope.maximumUncoverLimit = ($scope.dimension*$scope.dimension)-numOfMine;
 
+  $scope.getBoxClass = function(x, y) {
+    return '.box-' + x + '-' + y;
+  };
+
   //game finished. check for success/fail
   $scope.gameOver = function(x, y) {
-    if($scope.board[x][y] === '*'){
-      angular.element('.box-'+x+'-'+y).removeClass('covered').addClass('crash');
+    if($scope.board[x][y] === '*') {
+      angular.element($scope.getBoxClass(x, y)).removeClass('covered').addClass('crash');
       $scope.status = 'fail';
     }
     else{
       $scope.status = 'success';
     }
     angular.element('.mine').removeClass('covered').addClass('uncovered');
+    angular.element('.mine span').removeClass('hidden')
   };
 
   //show the value of the box to user
-  $scope.unCover = function(x, y){
+  $scope.unCover = function(x, y) {
 
     if(x >= 0 && x < $scope.dimension && y >= 0 && y < $scope.dimension &&
-      angular.element('.box-' + x + '-' + y).hasClass('covered')) {
+      angular.element($scope.getBoxClass(x, y)).hasClass('covered')) {
 
-      angular.element('.box-' + x + '-' + y).removeClass('covered').addClass('uncovered');
+      angular.element($scope.getBoxClass(x, y)).removeClass('covered').addClass('uncovered');
+      angular.element($scope.getBoxClass(x, y)+' span').removeClass('hidden');
 
       if ($scope.board[x][y] === 0) {
-        $scope.unCover(x - 1, y - 1);
-        $scope.unCover(x - 1, y);
-        $scope.unCover(x - 1, y + 1);
-        $scope.unCover(x, y - 1);
-        $scope.unCover(x, y + 1);
-        $scope.unCover(x + 1, y - 1);
-        $scope.unCover(x + 1, y);
-        $scope.unCover(x + 1, y + 1);
+        //unconver attached risk free boxes
+        edgeTraveler(x, y, $scope.unCover);
       }
+
       var currentlyUncovered  = angular.element('.mine.uncovered').length;
       if(currentlyUncovered >= $scope.maximumUncoverLimit){
         $scope.gameOver(x,y);
@@ -97,7 +99,7 @@ function GameController($scope)
   };
 
   //event handler upon clicking a specific box
-  $scope.boxClicked = function(x, y){
+  $scope.boxClicked = function(x, y) {
 
     if($scope.board[x][y] === '*'){
       //TODO verify success status
@@ -109,8 +111,8 @@ function GameController($scope)
   };
 
   //set flagged status for suspicious box
-  $scope.setFlagged = function(x,y){
-    angular.element('.box-'+x+'-'+y+' div.flag').addClass('glyphicon-flag');
+  $scope.setFlagged = function(x,y) {
+    angular.element($scope.getBoxClass(x, y)+' div.flag').addClass('glyphicon-flag');
   };
 
   //initialize game
@@ -118,8 +120,9 @@ function GameController($scope)
     $scope.status = '';
     angular.element('.mine').removeClass('uncovered crash').addClass('covered');
     angular.element('.mine div.flag').removeClass('glyphicon-flag');
+    angular.element('.mine span').addClass('hidden');
     $scope.board = [];
-    $scope.board = new Board($scope.dimension, numOfMine).boardMatrix;
+    $scope.board = new Board($scope.dimension, numOfMine, edgeTraveler).boardMatrix;
   };
 
   $scope.resetGame();
@@ -134,6 +137,23 @@ function GameController($scope)
  * Controller of the minesweeperApp
  */
 var app = angular.module('minesweeperApp');
+
+app.config(function($provide) {
+  $provide.factory('edgeTraveler', function(){
+    return function(x, y, callback){
+      //call to callback for all attached edges of the box
+      //validation of the x/y value must need to be done inside callback
+      callback(x - 1, y - 1);
+      callback(x - 1, y);
+      callback(x - 1, y + 1);
+      callback(x, y - 1);
+      callback(x, y + 1);
+      callback(x + 1, y - 1);
+      callback(x + 1, y);
+      callback(x + 1, y + 1);
+    };
+  });
+});
 
 //The main game controller definition and activity
 app.controller('GameCtrl', GameController);
